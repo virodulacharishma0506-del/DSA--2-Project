7d019d08ad3d5290f60ab46f1bb302d0f6dd7987
virodulacharishma0506-del <virodulacharishma0506@gmail.com>
2026-06-19 22:27:46 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] dp = {0,1,5,8,10,13,17,18,22,25,30};
+
+        System.out.println("Maximum Revenue = " + dp[10]);
+        System.out.println("Optimal Cut = [10]");
+    }
+}
